d034ac407e8d15dcd4789c8c53174f0d057fe706
Alexey Ilin <alexey.i.nn@gmail.com>
2023-10-07 14:03:11 +0800

    feat: env connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+console.log(process.env.POSTGRES_SYNCHRONIZE);
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'eth_transactions',
-      synchronize: true,
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: JSON.parse(process.env.POSTGRES_SYNCHRONIZE),
     }),
   ],
   controllers: [],
